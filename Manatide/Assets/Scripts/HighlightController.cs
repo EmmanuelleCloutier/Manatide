335f2f0884e4c6a90ead1713fcfeed4a27bbeb28
EmmanuelleCloutier <124931881+EmmanuelleCloutier@users.noreply.github.com>
2026-09-18 17:02:22 -0400

    main menu buttons workings

--- Manatide/Assets/Scripts/HighlightController.cs
+++ Manatide/Assets/Scripts/HighlightController.cs
@@ -1,65 +1,28 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class HighlightController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject highlight; // L’image cadre à afficher
-    private RectTransform highlightRect;
-    private RectTransform rectTransform;
+    [Header("Button Sprites")]
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite highlightedSprite;
 
-    public Vector2 padding = new Vector2(20f, 20f); // marge autour du bouton
+    private Image buttonImage;
 
-    void Start()
+    private void Awake()
     {
-        highlightRect = highlight.GetComponent<RectTransform>();
-        rectTransform = GetComponent<RectTransform>();
-
-        // Desactiver le raycast pour eviter clignotement
-        Image highlightImage = highlight.GetComponent<Image>();
-        if (highlightImage != null)
-        {
-            highlightImage.raycastTarget = false;
-        }
+        buttonImage = GetComponent<Image>();
+        buttonImage.sprite = normalSprite;
     }
-
-
-    void Update()
-    {
-        // Si la souris ne survole plus aucun objet UI, cacher le highlight
-        if (!EventSystem.current.IsPointerOverGameObject())
-        {
-            highlight.SetActive(false);
-        }
-    }
-
-   
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (highlight != null)
-        {
-            highlight.SetActive(true);
-
-            // Convertir la position du bouton en position dans la Canvas
-            Vector3 worldPos = rectTransform.position;
-
-            // Positionner le highlight au même endroit
-            highlightRect.position = worldPos;
-
-            // Ajuster la taille en ajoutant un padding
-            highlightRect.sizeDelta = rectTransform.sizeDelta + padding;
-
-            // S’assurer que le pivot est bien au centre
-            highlightRect.pivot = new Vector2(0.5f, 0.5f);
-        }
+        buttonImage.sprite = highlightedSprite;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (highlight != null)
-        {
-            highlight.SetActive(false);
-        }
+        buttonImage.sprite = normalSprite;
     }
 }
